7ac306e0788ade3da12d367d9a3a828ecebc3952
cathoDr-immalle <catho.driesen@immalle.be>
2017-10-20 11:35:39 +0200

    voorbeelden

--- Loop Voorbeeldjes/Program.cs
+++ Loop Voorbeeldjes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Loop_Voorbeeldjes
 {
@@ -10,6 +11,39 @@ namespace Loop_Voorbeeldjes
     {
         static void Main(string[] args)
         {
+            int x = 10;
+            int som = 0;
+            string invoer = "";
+
+            while (x>3)
+            {
+                x--;
+                Console.WriteLine(x);
+            }
+
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            
+            for (int i = 0; i <= 100000000; i++)
+            {
+                som += i;
+                Console.WriteLine(som);
+                Thread.Sleep(100);
+            }
+
+            while (invoer == "Hallo")
+            {
+                Console.WriteLine("Hallo!");
+                Console.ReadLine();
+
+                Console.WriteLine(x);
+
+            }
+
         }
     }
 }
